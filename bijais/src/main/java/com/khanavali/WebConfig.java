74e31a2c554c568f24ac5dea17a01551cf09526a
Sharana <sharanbm@gmail.com>
2013-10-27 08:05:45 +0000

    updated the gradle script to include deploy to tomcat task

--- bijais/src/main/java/com/khanavali/WebConfig.java
+++ bijais/src/main/java/com/khanavali/WebConfig.java
@@ -20,14 +20,16 @@ import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.khanavali.web.controller","com.khanavali.web.domain"})
+@ComponentScan(basePackages = { "com.khanavali.web.controller",
+		"com.khanavali.web.domain" })
 public class WebConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations(
+				"/resources/");
 	}
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 
@@ -35,49 +37,51 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		localeChangeInterceptor.setParamName("lang");
 		registry.addInterceptor(localeChangeInterceptor);
 	}
-	
+
 	@Bean
 	public LocaleResolver localeResolver() {
 
 		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
-		cookieLocaleResolver.setDefaultLocale(StringUtils.parseLocaleString("en"));
+		cookieLocaleResolver.setDefaultLocale(StringUtils
+				.parseLocaleString("en"));
 		return cookieLocaleResolver;
 	}
 
-  @Bean
-  public ServletContextTemplateResolver templateResolver() {
-    ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
-    resolver.setPrefix("/WEB-INF/views/");
-    resolver.setSuffix(".html");
-    //NB, selecting HTML5 as the template mode.
-    resolver.setTemplateMode("HTML5");
-    resolver.setCacheable(false);
-    return resolver;
+	@Bean
+	public ServletContextTemplateResolver templateResolver() {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".html");
+		// NB, selecting HTML5 as the template mode.
+		resolver.setTemplateMode("HTML5");
+		resolver.setCacheable(false);
+		return resolver;
 
-  }
+	}
 
-  public SpringTemplateEngine templateEngine() {
-    SpringTemplateEngine engine = new SpringTemplateEngine();
-    engine.setTemplateResolver(templateResolver());
-    return engine;
-  }
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
 
 	@Bean
 	public ViewResolver viewResolver() {
 
-    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
 		viewResolver.setTemplateEngine(templateEngine());
-    viewResolver.setOrder(1);
-    viewResolver.setViewNames(new String[]{"*"});
-    viewResolver.setCache(false);
+		viewResolver.setOrder(1);
+		viewResolver.setViewNames(new String[] { "*" });
+		viewResolver.setCache(false);
 		return viewResolver;
 	}
-	
+
 	@Bean
 	public MessageSource messageSource() {
 
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasenames("classpath:messages/messages", "classpath:messages/validation");
+		messageSource.setBasenames("classpath:messages/messages",
+				"classpath:messages/validation");
 		// if true, the key of the message will be displayed if the key is not
 		// found, instead of throwing a NoSuchMessageException
 		messageSource.setUseCodeAsDefaultMessage(true);
