292de515a62a175835bb417a33de2fa12e3dd252
melkotek <krishna.melkote@gmail.com>
2013-11-02 13:30:51 +0000

    Customer for core

--- bijais/src/main/java/com/khanavali/config/CoreConfig.java
+++ bijais/src/main/java/com/khanavali/config/CoreConfig.java
@@ -1,14 +1,17 @@
 package com.khanavali.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.khanavali.core.services.CustomerEventHandler;
+import com.khanavali.core.services.CustomerService;
 import com.khanavali.core.services.MenuEventHandler;
 import com.khanavali.core.services.MenuService;
 import com.khanavali.core.services.OrderEventHandler;
 import com.khanavali.core.services.OrderService;
+import com.khanavali.persistence.services.CustomerPersistenceService;
 import com.khanavali.persistence.services.MenuPersistenceService;
 import com.khanavali.persistence.services.OrderPersistenceService;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CoreConfig {
@@ -16,8 +19,17 @@ public class CoreConfig {
 	public MenuService menuService(MenuPersistenceService menuPersistenceService) {
 		return new MenuEventHandler(menuPersistenceService);
 	}
+  
+	
   @Bean
   public OrderService orderService(OrderPersistenceService orderPersistenceService) {
     return new OrderEventHandler(orderPersistenceService);
   }
+  
+  @Bean
+  public CustomerService customerService(CustomerPersistenceService customerPersistenceService) {
+    return new CustomerEventHandler(customerPersistenceService);
+  }
+  
+  
 }
---
+++ bijais/src/main/java/com/khanavali/core/services/CustomerEventHandler.java
@@ -0,0 +1,37 @@
+package com.khanavali.core.services;
+
+import com.khanavali.events.customer.AllCustomersEvent;
+import com.khanavali.events.customer.CreateCustomerEvent;
+import com.khanavali.events.customer.CustomerDetailsEvent;
+import com.khanavali.events.customer.RequestAllCustomersEvent;
+import com.khanavali.events.customer.RequestCustomerDetailsEvent;
+import com.khanavali.persistence.services.CustomerPersistenceService;
+
+public class CustomerEventHandler implements CustomerService {
+
+	private CustomerPersistenceService customerPersistenceService;
+
+	public CustomerEventHandler(
+			CustomerPersistenceService customerPersistenceService) {
+		this.customerPersistenceService = customerPersistenceService;
+	}
+
+	@Override
+	public AllCustomersEvent requestAllCustomerDetails(
+			RequestAllCustomersEvent requestAllCustomersEvent) {
+		return this.customerPersistenceService.requestAllCustomers(requestAllCustomersEvent);
+	}
+
+	@Override
+	public CustomerDetailsEvent requestCustomerDetails(
+			RequestCustomerDetailsEvent requestCustomerDetailsEvent) {
+		return this.customerPersistenceService.requestCustomerDetails(requestCustomerDetailsEvent);
+	}
+
+	@Override
+	public CustomerDetailsEvent createCustomer(
+			CreateCustomerEvent createCustomerEvent) {
+		return this.createCustomer(createCustomerEvent);
+	}
+
+}
